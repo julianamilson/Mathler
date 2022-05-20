5d47d482e5bcae1fd2a8630fbb149960abf86f49
Luis <lp_santos29@outlook.com>
2022-05-20 16:07:51 -0300

    front tá montando a mensagem em json pra enviar pro back lindamente

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("Hello World!");
-
+	
+	var guess = "";
 	let availableChars = 6;
+	
 	let guessChars = document.querySelectorAll("#guess")
-	console.log(guessChars);
-
+	
 	document.addEventListener('keydown', (event) => {
 		var code = event.code;
 		var key = event.key;
-		if (code == "Enter") {
+		
+		if (code == "Enter" && availableChars == 0) {
 			// handleSubmitWord()
-			console.log("Enter detectado")
+			var temp = `{"guess": "${guess}"}`
+			var fileJson = JSON.parse(temp)
+			fetch(
+				`http://localhost:5000/clues`,
+				{
+					method: "POST",
+					body: fileJson
+				}
+			).then((res) => {
+				if (!res.ok) {
+					throw Error()
+				}
+				console.log("deu bom!!");
+				console.log(res.body);
+			}).catch(() => {
+				window.alert("Deu ruim, body no console log");
+				console.log(res.body);
+			})
 			return;
 		}
 		
 		if (code == "Backspace" || code == "Delete") {
-			// handleDeleteLetter()
-			console.log("Backspace detectado")
+			if (availableChars < 6)
+			{
+				guessChars[5 - availableChars].textContent = "";
+				guess = guess.slice(0, -1);
+				availableChars++;
+			}
 			return;
 		}
 		
 		if (/^[0-9]$/i.test(key) || /^[-*+/]$/i.test(key)) {
 			// updateGuessedWords(key);
-			console.log("Número ou sinal detectado")
 			if (availableChars)
 			{
 				guessChars[6 - availableChars].textContent = key;
-				// document.getElementById("guess").textContent = key;
+				guess += key;
 				availableChars--;
 			}
 		}
--- webserver.py
+++ webserver.py
@@ -30,4 +30,4 @@ def testgoGetClue():
 	
 
 if __name__== "__main__":
-	app.run(debug=True)
+	app.run()
