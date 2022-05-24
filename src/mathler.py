a17b2f50793a2556f8f387f859731ee63d7691a7
Luis <lp_santos29@outlook.com>
2022-05-24 17:42:45 -0300

    add unittest

--- src/mathler.py
+++ src/mathler.py
@@ -9,6 +9,8 @@ EQUATION_SIZE = 6
 def mathler(guess):
 	equation = "39+1+2"
 
+	if (guess == None or guess == ""):
+		return ("Empty input!")
 	if (len(guess) != EQUATION_SIZE):
 		return (f"Wrong size! Has to be {EQUATION_SIZE} long.")
 	elif (IsAValidCharacter (guess) == False):
@@ -23,25 +25,27 @@ def mathler(guess):
 		return(getClue(equation, guess))
 
 def isValidToEval (guess):
-
 	trigger = False
 
+	if (guess == "" or guess == None):
+		return False
 	for i in range (len(guess)):
 		if (i < 5 and guess[i] == "0" and isNumber (guess[i + 1]) == True and trigger == False):
 			return (False)
 		elif (i < 5 and guess[i] == "/" and guess[i + 1] == "0"):
 			return (False)
-		elif (isNumber (i) == True):
+		elif (isNumber (guess[i]) == True):
 			trigger = True
-		elif (isNumber (i) == False):
+		elif (isNumber (guess[i]) == False):
 			trigger = False
-
-		
 	return (True)
 
 def getClue(secret_equation, guess):
 	clue = ""
 	i = 0
+
+	if (secret_equation == None or guess == None or guess == "" or secret_equation == ""):
+		return ("XXXXXX")
 	# loop externo, no string de chute
 	for i in range(EQUATION_SIZE):
 		j = 0
@@ -58,7 +62,8 @@ def getClue(secret_equation, guess):
 	return clue
 
 def IsAValidCharacter(guess):
-
+	if (guess == None or guess == ""):
+		return False
 	for element in guess:
 		if (isNumber(element) == False and isSign(element) == False):
 			return False
@@ -67,6 +72,8 @@ def IsAValidCharacter(guess):
 def isNumber(element):
 	valid_characters = "1234567890"
 
+	if (element == None or element == ""):
+		return False
 	for x in valid_characters:
 		if (element == x):
 			return True
@@ -75,15 +82,18 @@ def isNumber(element):
 def isSign(character):
 	list_of_Sign = "-+/*"
 
+	if (character == None or character == ""):
+		return False
 	for i in list_of_Sign:
 		if character == i:
 			return True
 	return False
 
 def isSignConsistent(guess):
-
 	prev = 0
 
+	if (guess == None or guess == ""):
+		return False
 	if (guess[0] == "/" or guess[0] == "*"):
 		return False
 	if isSign(guess[len(guess) - 1]):
--- test/test_mathler.py
+++ test/test_mathler.py
@@ -1,5 +1,363 @@
 import unittest
 from src.mathler import *
 
-class Test_mathler(unittest.TestCase):
-	def	
+class test_mathler_isSign(unittest.TestCase):
+		
+	def	test_isSign_input_empty_returns_false(self):
+		input = None
+		expected_response = False
+
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+		
+	def	test_isSign_input_empty_returns_false(self):
+		input = ""
+		expected_response = False
+
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+		
+	def	test_isSign_times_returns_true(self):
+		input =  "*"
+		expected_response = True
+
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+		
+	def	test_isSign_division_and_number_returns_false(self):
+		input =  "/0"
+		expected_response = False
+
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+		
+	def	test_isSign__number_and_division_returns_false(self):
+		input =  "0/"
+		expected_response = False
+
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isSign_single_division_returns_true(self):
+		input = "/"
+		expected_response = True
+
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isSign_string_returns_false(self):
+		input = "Hello!"
+		expected_response = False
+		
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isSign_single_char_returns_false(self):
+		input = "a"
+		expected_response = False
+		
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isSign_number_returns_false(self):
+		input = "1"
+		expected_response = False
+		
+		response = isSign(input)
+		self.assertEqual(response, expected_response)
+
+class test_mathler_isNumber(unittest.TestCase):
+	
+	def	test_isNumber_input_none_returns_false(self):
+		input = None
+		expected_response = False
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+	
+	def	test_isNumber_input_empty_returns_false(self):
+		input = ""
+		expected_response = False
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isNumber_number_1_returns_true(self):
+		input = "1"
+		expected_response = True
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isNumber_number_9_returns_true(self):
+		input = "9"
+		expected_response = True
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isNumber_char_a_returns_false(self):
+		input = "a"
+		expected_response = False
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+
+	def	test_isNumber_sign_times_returns_false(self):
+		input = "*"
+		expected_response = False
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+	
+	def	test_isNumber_sign_minus_returns_false(self):
+		input = "-"
+		expected_response = False
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isNumber_string_returns_false(self):
+		input = "2ef2e*0"
+		expected_response = False
+		
+		response = isNumber(input)
+		self.assertEqual(response, expected_response)
+
+class test_mathler_IsAValidCharacter(unittest.TestCase):
+	
+	def test_isAValidCharacter_input_none_returns_false(self):
+		input = None
+		expected_response = False
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+	
+	def test_isAValidCharacter_input_empty_returns_false(self):
+		input = ""
+		expected_response = False
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+	
+	def test_isAValidCharacter_valid_equation_returns_true(self):
+		input = "31+1+2"
+		expected_response = True
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isAValidCharacter_char_and_number_equation_returns_false(self):
+		input = "31+a+2"
+		expected_response = False
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isAValidCharacter_start_letter_numbers_equation_returns_false(self):
+		input = "R+1242"
+		expected_response = False
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isAValidCharacter_no_sign_returns_true(self):
+		input = "542679"
+		expected_response = True
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isAValidCharacter_only_signs_returns_true(self):
+		input = "+-/**-"
+		expected_response = True
+		
+		response = IsAValidCharacter(input)
+		self.assertEqual(response, expected_response)
+
+
+class test_mathler_isSignConsistent(unittest.TestCase):
+	
+	def test_isSignConsistent_input_none_returns_false(self):
+		input = None
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+	
+	def test_isSignConsistent_input_empty_returns_false(self):
+		input = ""
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+	
+	def test_isSignConsistent_only_signals_returns_false(self):
+		input = "+-/**-"
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isSignConsistent_two_consecutive_signs_returns_false(self):
+		input = "40++2"
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isSignConsistent_initial_times_sign_returns_false(self):
+		input = "*40++2"
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+	
+	def test_isSignConsistent_initial_plus_sign_returns_true(self):
+		input = "+40+12"
+		expected_response = True
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)	
+
+	def test_isSignConsistent_initial_minus_sign_returns_true(self):
+		input = "-40+11"
+		expected_response = True
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isSignConsistent_initial_minus_sign_returns_true(self):
+		input = "-40+11"
+		expected_response = True
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isSignConsistent_with_minus_sign_at_end_returns_false(self):
+		input = "40+11-"
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)		
+
+	def test_isSignConsistent_with_divide_sign_at_end_returns_false(self):
+		input = "40+11/"
+		expected_response = False
+		
+		response = isSignConsistent(input)
+		self.assertEqual(response, expected_response)
+
+class test_mathler_isValidToEval(unittest.TestCase):
+	
+	def test_isValidToEval_input_none_returns_false(self):
+		input = None
+		expected_response = False
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isValidToEval_input_empty_returns_false(self):
+		input = ""
+		expected_response = False
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isValidToEval_allZero_returns_false(self):
+		input = "000000"
+		expected_response = False
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isValidToEval_divided_by_Zero_returns_false(self):
+		input = "42/0-1"
+		expected_response = False
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isValidToEval_number_after_zero_returns_false(self):
+		input = "042-32"
+		expected_response = False
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+		
+	def test_isValidToEval_zeroPlus_returns_true(self):
+		input = "0+42-1"
+		expected_response = True
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isValidToEval_zero_divided_by_number_returns_true(self):
+		input = "0/43-1"
+		expected_response = True
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isValidToEval_one_hundred_Division_returns_True(self):
+		input = "100/20"
+		expected_response = True
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isValidToEval_one_thousand_Division_returns_True(self):
+		input = "1000/2"
+		expected_response = True
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+
+	def test_isValidToEval_number_4_after_zero_returns_false(self):
+		input = "12+040"
+		expected_response = False
+		
+		response = isValidToEval(input)
+		self.assertEqual(response, expected_response)
+
+class test_mathler_getClue(unittest.TestCase):
+	
+	def test_getClue_input_empty_equation_123_plus_45_returns_XXXXXX(self):
+		input = ""
+		equation = "123+45"
+		expected_response = "XXXXXX"
+		
+		response = getClue(equation, input)
+		self.assertEqual(response, expected_response)
+	
+	def test_getClue_input_123_plus_45_equation_45_minus_123_returns_TTTXTT(self):
+		input = "123+45"
+		equation = "45-123"
+		expected_response = "TTTXTT"
+		
+		response = getClue(equation, input)
+		self.assertEqual(response, expected_response)
+	
+	def test_getClue_input_123_plus_45_equation_123_plus_45_returns_CCCCCC(self):
+		input = "123+45"
+		equation = "123+45"
+		expected_response = "CCCCCC"
+		
+		response = getClue(equation, input)
+		self.assertEqual(response, expected_response)
+	
+	def test_getClue_input_67_minus_9_times_8_equation_123_plus_45_returns_XXXXXX(self):
+		input = "67-9*8"
+		equation = "123+45"
+		expected_response = "XXXXXX"
+		
+		response = getClue(equation, input)
+		self.assertEqual(response, expected_response)
+	
+	def test_getClue_input_none_returns_XXXXXX(self):
+		input = None
+		equation = "123+45"
+		expected_response = "XXXXXX"
+		
+		response = getClue(equation, input)
+		self.assertEqual(response, expected_response)
